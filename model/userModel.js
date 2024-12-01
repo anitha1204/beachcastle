c43e3dc451ff47f91ddba7cb8393867ee1dc9d91
anitha1204 <selvam12042003@gmail.com>
2024-12-02 00:12:07 +0530

    main

--- model/userModel.js
+++ model/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userbeachcastleSchema = new mongoose.Schema({
-  userName: { type: String, required: true },
+  username: { type: String, required: true },
   mobile: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   enquiry: { type: String, required: true },
